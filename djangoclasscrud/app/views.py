0c5ec6678d09702a01de1544542c672bbc027da6
janakhpon <jnovaxer@gmail.com>
2019-09-03 11:25:02 +0630

    GDEV002

--- djangoclasscrud/app/views.py
+++ djangoclasscrud/app/views.py
@@ -10,4 +10,30 @@ class IndexView(TemplateView):
 
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
-        context[]
+        context['hereval'] = "HERE IS SERVER SIDE VALUE"
+        return context
+
+class SchoolListView(ListView):
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_details'
+    model = models.School
+    template_name = 'app/detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ("name", "principal", "location")
+    model = model.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ("name", "principal")
+    model = models.School
+
+class SchooDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("app:list")
+
+class CBView(View):
+    def get(self,request):
+        return HttpResponse('Class Based View are Cool!')
